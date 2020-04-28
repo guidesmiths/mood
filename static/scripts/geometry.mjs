4cdb0161e4dd31488fe2e8194a99f524c481408a
Daniel Herrero <danielherrerohernando@gmail.com>
2020-04-28 17:57:10 +0200

    refactor: improving names for the sake of readability
    ...
    
    noticket

--- static/scripts/geometry.mjs
+++ static/scripts/geometry.mjs
@@ -2,25 +2,25 @@ import { imageDimensions, imageTags } from '../data/index.mjs'
 
 const numOfTags = Object.keys(imageTags).length;
 
-const getDistance = (h,v) => ((h**2+v**2)**(1/2));
+const getDistance = (horizontal,vertical) => ((horizontal**2+vertical**2)**(1/2));
 
 const calcArcsin = (a1,a2) => Math.asin(Math.abs(a1)/getDistance(a1,a2));
 
-const getAngle = (h,v) => {
-  if(h>=0 && v>=0) return calcArcsin(h,v)
-  if(h>=0 && v<0) return calcArcsin(v,h)+Math.PI/2
-  if(h<0 && v<0) return calcArcsin(h,v)+Math.PI
-  if(h<0 && v>=0) return calcArcsin(v,h)+3/2*Math.PI
+const getAngle = (horizontal,vertical) => {
+  if(horizontal>=0 && vertical>=0) return calcArcsin(horizontal,vertical)
+  if(horizontal>=0 && vertical<0) return calcArcsin(vertical,horizontal)+Math.PI/2
+  if(horizontal<0 && vertical<0) return calcArcsin(horizontal,vertical)+Math.PI
+  if(horizontal<0 && vertical>=0) return calcArcsin(vertical,horizontal)+3/2*Math.PI
 };
 
-const getSpecifity = (tags, distance) => {
+const getSpecificity = (tags, distance) => {
   if (distance < imageDimensions.firstLevel) return tags.slice(0,1)
   if (distance < imageDimensions.secondLevel) return tags.slice(0,2)
   if (distance < imageDimensions.thirdLevel) return tags
   return []
 }
 
-export const getTags = (h,v) => {
-  const tagSlice = Math.floor(getAngle(h,v)/(2*Math.PI/numOfTags))
-  return getSpecifity(imageTags[tagSlice], getDistance(h,v))
+export const getTags = (horizontal,vertical) => {
+  const tagSlice = Math.floor(getAngle(horizontal,vertical)/(2*Math.PI/numOfTags))
+  return getSpecificity(imageTags[tagSlice], getDistance(horizontal,vertical))
 }
